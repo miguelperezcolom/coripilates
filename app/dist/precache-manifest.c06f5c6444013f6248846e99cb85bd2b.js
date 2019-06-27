4c1c38548b574f988c4dd4121cf1749b861eddc8
miguelperezcolom <miguelperezcolom@gmail.com>
2019-06-27 22:39:50 +0200

    aa

---
+++ app/dist/precache-manifest.c06f5c6444013f6248846e99cb85bd2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c25c7aa2d071c7e190fa",
+    "url": "/js/chunk-vendors.790135bc.js"
+  },
+  {
+    "revision": "3ad67132cbc336d27630",
+    "url": "/js/app.27137a89.js"
+  },
+  {
+    "revision": "eaf63216825b5e7239c5e8faebb5da64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3ad67132cbc336d27630",
+    "url": "/css/app.e6fcb570.css"
+  }
+];
